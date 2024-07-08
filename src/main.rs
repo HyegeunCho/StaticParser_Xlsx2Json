1f5f6200ee4ec45be6f3538033d4551be8b82885
HyegeunCho <hgcho84.kr@gmail.com>
2024-07-08 23:52:28 +0900

    [추가] sede_json을 활용 json 파일 생성

--- src/main.rs
+++ src/main.rs
@@ -31,13 +31,10 @@ fn cell_data_to_string(data: &Data) -> String {
 fn get_headers_from_sheet(sheet_range: &calamine::Range<Data>) -> HashMap<i64, String> {
 
     let mut result: HashMap<i64, String> = HashMap::new();
-
     let mut cell_index: i64 = 0;
-
-
     let mut rows = sheet_range.rows();
-    if let Some(first_row) = rows.next() {
 
+    if let Some(first_row) = rows.next() {
         for cell in first_row {
             let cell_value = cell_data_to_string(cell);
             if !cell_value.starts_with('_') {
@@ -51,10 +48,33 @@ fn get_headers_from_sheet(sheet_range: &calamine::Range<Data>) -> HashMap<i64, S
     result
 } 
 
+/// Hashmap 구조
+/// {sheet_name: [{ID: 0, KEY_A: 1, KEY_B: 2}, ...], sheet_name2: [...]}
+
+fn serialize_to_json(hash_map: &HashMap<String, Vec<HashMap<String, String>>>) -> Result<(), Box<dyn Error>> {
+
+    let mut map = HashMap::new();
+    map.insert("key1", "value1");
+    map.insert("key2", "value2");
+    map.insert("key3", "value3");
+
+    // HashMap을 JSON 문자열로 변환
+    let json = serde_json::to_string_pretty(&hash_map).expect("Failed to convert HashMap to JSON");
+
+    // JSON 문자열 출력
+    // println!("{}", json);
+
+    let mut file = File::create("bd_static.json")?;
+    file.write_all(json.as_bytes())?;
+    
+    Ok(())
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let path = "bd_static.xlsx";
     let mut workbook: Xlsx<_> = open_workbook(path)?;
 
+    let mut static_data: HashMap<String, Vec<HashMap<String, String>>> = HashMap::new();
     // print sheet names
     let mut sheet_index: i64 = 0;
     for sheet_name in workbook.sheet_names().to_owned() {
@@ -62,119 +82,42 @@ fn main() -> Result<(), Box<dyn Error>> {
         if sheet_name.starts_with('_') {
             continue;
         }
-        println!("[{}] {}", sheet_index, sheet_name);
+        // println!("[{}] {}", sheet_index, sheet_name);
 
-        if sheet_index == 4 {
-            let range_result = workbook.worksheet_range(&sheet_name);
+        
+        let mut sheet_data: Vec<HashMap<String, String>> = Vec::new();
+        let range_result = workbook.worksheet_range(&sheet_name);
+        match range_result {
+            Ok(range) => {
 
-            let mut header: HashMap<i64, String> = HashMap::new();
-            match range_result {
-                Ok(range) => {
+                let header_from_fn = get_headers_from_sheet(&range);
+                let mut header_keys: Vec<i64> = header_from_fn.keys().map(|i| i.clone()).collect();
+                header_keys.sort();
 
-                    let header_from_fn = get_headers_from_sheet(&range);
-                    let mut header_keys: Vec<i64> = header_from_fn.keys().map(|i| i.clone()).collect();
-                    header_keys.sort();
-
-                    let mut row_index: i64 = 0;
-                    for row in range.rows() {
-                        
-                        if row_index > 0 {
-
-                            for hIndex in &header_keys {
-                        
-                                let header_name = header_from_fn.get(&hIndex).unwrap_or(&("".to_string())).clone();
-                                let cell_value = cell_data_to_string(row.get(*hIndex as usize).ok_or("".to_string()).unwrap());
-                                println!("[{:?} : {:?}]", header_name, cell_value);
-                                
-
-                                // if let Some(cell_value) = row.get(*hIndex as usize).map(|c| cell_data_to_string(c)) {
-                                //     println!("[{:?}] {} {:?}", header_name, hIndex, cell_value);
-                                // }
-                                // else{
-                                    
-                                // }
-                            }
-
+                let mut row_index: i64 = 0;
+                for row in range.rows() {
+                    
+                    let mut row_data: HashMap<String, String> = HashMap::new();
+                    if row_index > 0 {
+                        for hIndex in &header_keys {
+                            let header_name = header_from_fn.get(&hIndex).unwrap_or(&("".to_string())).clone();
+                            let cell_value = cell_data_to_string(row.get(*hIndex as usize).ok_or("".to_string()).unwrap());
+                            row_data.insert(header_name.clone(), cell_value.clone());
                         }
-                        row_index = row_index + 1;
+                        sheet_data.push(row_data);
                     }
-                },
-                Err(e) => {
-                    println!("에러 발생");
+                    row_index = row_index + 1;
                 }
+            },
+            Err(e) => {
+                println!("에러 발생");
             }
         }
+        
+        static_data.insert(sheet_name.clone(), sheet_data);
+        serialize_to_json(&static_data);
     }
 
     
-
-
-
-    // for sheet_name in workbook.sheet_names().to_owned() {
-        
-    //     if sheet_name.starts_with('_') {
-    //         continue;  
-    //     }
-
-    //     let range_result = workbook.worksheet_range(&sheet_name);
-    //     match  range_result {
-    //         Ok(range) => {
-    //             let mut index : i64 = 0;
-    //             for row in range.rows() {
-    //                 for cell in row {
-    //                     println!("{:?}", cell.get_string());
-    //                 }
-    //                 index = index + 1;
-    //             }
-                
-    //             // let mut rangeIter = RangeDeserializerBuilder::new().from_range(&range)?;
-    //             // if let Some(result) = rangeIter.next() {
-    //             //     let (label, value): (String, f64) = result?;
-    //             // }
-    //         },
-    //         Err(e) => {
-                
-    //         }
-    //     }
-        
-    //     // let range = workbook.worksheet_range(&sheet_name)
-    //     //     .ok_or(Error::Msg("Cannot find " + &sheet_name))??;
-    //     // 
-    //     // let mut rangeIter = RangeDeserializerBuilder::new().from_range(&range)?;
-    //     // if let Some(result) = rangeIter.next() {
-    //     //     let (label, value): (String, f64) = result?;
-    //     //     assert_eq!(label, "celsius");
-    //     //     assert_eq!(value, 22.2222);
-    //     //     Ok(());
-    //     // } 
-         
-    //     // if let Some(Ok(range)) = workbook.worksheet_range(&sheet_name) {
-    //     //     let mut rows = range.rows();
-    //     // 
-    //     //     if let Some(headers) = rows.next() {
-    //     //         let headers: Vec<String> = headers.iter().map(|h| h.to_string()).collect();
-    //     // 
-    //     //         let mut sheet_data = Vec::new();
-    //     // 
-    //     //         for row in rows {
-    //     //             let mut row_data = HashMap::new();
-    //     //             for (header, cell) in headers.iter().zip(row.iter()) {
-    //     //                 row_data.insert(header.clone(), cell.clone());
-    //     //             }
-    //     //             sheet_data.push(row_data);
-    //     //         }
-    //     // 
-    //     //         // sheets.push(Sheet {
-    //     //         //     name: sheet_name.clone(),
-    //     //         //     data: sheet_data,
-    //     //         // });
-    //     //     }
-    //     // }
-    // }
-
-    // let json = serde_json::to_string_pretty(&sheets)?;
-    // let mut file = File::create("output.json")?;
-    // file.write_all(json.as_bytes())?;
-
     Ok(())
 }
